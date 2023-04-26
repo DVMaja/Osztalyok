f69bb513c078f4a8bba5be19106a5b37559d8f77
DVMaja <majadreilinger@gmail.com>
2023-04-26 10:49:07 +0200

    elemek megjelenítése az index.html - en

--- Szemely.js
+++ Szemely.js
@@ -1,14 +1,42 @@
-class Szemely{
-    /**Az osztály egy sablon amivel Személyeket hozhatunk létre */
+class Szemely {
+  /**Az osztály egy sablon amivel Személyeket hozhatunk létre */
+  #szuletesiNev; //ez egy privát adattag
 
-    //javascriptben csak 1db konstruktor van
-    constructor(nev, szulDatum){
-        /**a konstruktórban beállítjuk az adattagokat */
-        this.nev = nev;
-        this.szulDatum =  szulDatum;
-        /**this az a konkrét osztálypéldányra mutat , azaza a konkrét  */
-    }
+  //javascriptben csak 1db konstruktor van
+  constructor(nev, szulDatum, SzuloElem) {
+    /**a konstruktórban beállítjuk az adattagokat */
+    this.nev = nev; //publikus adattag, azaz kívűlről eléerhető
+    this.szulDatum = szulDatum;
+    this.setSzuletesiNev(nev);
+    /**this az a konkrét osztálypéldányra mutat , azaza a konkrét objektumra */
 
+    /**megjelenítem az elemet a html oldalon */
+    SzuloElem.append(`
+    <div class="szemely">
+    <h3>${this.#szuletesiNev}</h3>
+    <p>${this.kor()}
+    </div>
+    `);
+  }
+
+  /**Privát adatagokat csak ezen keresztűl érhetek el */
+  getSzuletesinev() {
+    return this.#szuletesiNev;
+  }
+
+  //privát adattag ellenőrzött módosítására használjuk
+  setSzuletesiNev(ujnev) {
+    //a névben ne lehessen szám, nagybetűvel kezdődjön, csakadott nevek közül lehessen választani.
+    this.#szuletesiNev = ujnev;
+  }
+
+  kor() {
+    const d = new Date();
+    let year = d.getFullYear();
+    let aktualisKor = year - this.szulDatum;
+
+    return aktualisKor;
+  }
 }
 
 export default Szemely;
--- main.js
+++ main.js
@@ -4,6 +4,26 @@ $(function () {
     console.log("Hello World!");
 
     /**példányosítás azaz a sablon alapján egy konkrét objektum létrehozása */
-    const szemely1 = new Szemely("Béla", 1969)
-    console.log(szemely1);
+
+    const SzuloElem = $('.tarolo');
+
+    const szemely1 = new Szemely("Béla", 1969, SzuloElem)
+    //console.log(szemely1);
+    //console.log(szemely1.nev);
+    //console.log(szemely1.szulDatum);
+    szemely1.pontszam=12;
+    //console.log(szemely1.pontszam);
+    /**JS-ben futásidőben tudom módsítani az adattagokat! A JAVA nem ilyen */
+    
+    const szemely2 = new Szemely("Mucika", 1931, SzuloElem)
+    /**JAvascriptben az azonos osztályból példányosított elemek nem biztos , hogy 
+     * ugyanazokkal a tulajdonságokkal rendelkeznek
+     */
+
+    //console.log(szemely2.nev);
+    szemely2.nev = "Jolán";
+    //.log(szemely2.nev);
+    //log("SZületési név: " +szemely2.getSzuletesinev());
+    //console.log(szemely2.#szuletesiNev); privát adatagot nem tudok kívűlről elérni    
+    //console.log(szemely2.kor());
 })
